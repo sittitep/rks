a543b51be0b722c087dd55d70ddbc39b868a6d1d
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2018-10-17 19:01:06 +0700

    Add default queue and retry to worker

--- lib/rks/worker.rb
+++ lib/rks/worker.rb
@@ -1,6 +1,7 @@
 module RKS
   class Worker
     include Sidekiq::Worker
+    sidekiq_options retry: 3, queue: :normal
 
     def self.children
       ObjectSpace.each_object(Class).select { |klass| klass < self }
