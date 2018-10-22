553dcf3c2043ce7a0d4db0ec46e48655d174cb91
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2018-10-22 13:34:54 +0700

    Display args for sidekiq log

--- lib/rks/sidekiq.rb
+++ lib/rks/sidekiq.rb
@@ -4,15 +4,15 @@ module Sidekiq
   module LogStash
     def perform(*args)
       correlation_id = args[0]["correlation_id"]
-      content = args[0]["content"]
+      args = args[0]
       begin
         duration = Benchmark.measure {
-          Application.logger.info correlation_id: correlation_id, status: "started", worker: self.class.name, jid: self.jid, content: content
+          Application.logger.info correlation_id: correlation_id, status: "started", worker: self.class.name, jid: self.jid, args: args
           super(*args)
         }
         Application.logger.info correlation_id: correlation_id, status: "finished", worker: self.class.name, jid: self.jid, duration: duration.real.round(3)
       rescue Exception => e
-        Application.logger.fatal correlation_id: correlation_id, status: "failed", worker: self.class.name, jid: self.jid, content: content, error_message: e.message, error_detail: e.backtrace
+        Application.logger.fatal correlation_id: correlation_id, status: "failed", worker: self.class.name, jid: self.jid, args: args, error_message: e.message, error_detail: e.backtrace
         raise e
       end
     end
