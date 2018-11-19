1f6d3051ada1cdd6517c41fc614f511339b43b37
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2018-11-19 13:30:57 +0700

    Clean up event module

--- lib/rks/event.rb
+++ lib/rks/event.rb
@@ -1,27 +1,4 @@
 module RKS
-  class Event
-    extend ApplicationHelper
-
-    def self.event(args, &block)
-      Application.events[args[:name]] = Proc.new do
-        Application.current.payload = if args[:decoding] == true || args[:decoding] == nil
-          decode_message_value(current_event, current_payload)
-        else
-          JSON.parse(current_payload)
-        end
-
-        Application.logger.info correlation_id: current_correlation_id, status: "started", event: current_event, payload: current_payload
-        yield(current_payload)
-      end
-    end
-
-    def self.decode_message_value(topic, value)
-      camelized_topic = camelize(topic)
-      Application.avro_registry.decode(value, schema_name: camelized_topic, namespace: "events.#{camelized_topic}")
-    end
-
-    def self.camelize(str)
-      str.split('-').collect(&:capitalize).join
-    end
+  module Event
   end
 end
