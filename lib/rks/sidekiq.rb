20f0099caf7d5650027ed6e2072c66efc8b0836d
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2018-11-20 10:50:05 +0700

    Update sidekiq

--- lib/rks/sidekiq.rb
+++ lib/rks/sidekiq.rb
@@ -1,14 +1,12 @@
 require 'sidekiq'
+require 'sidekiq/processor'
 
-module Sidekiq
-  class Processor
-    # alias_method :original_execute_job, :execute_job
-
-    def execute_job(worker, cloned_args)
-      correlation_id = cloned_args[0]["correlation_id"]
-      Application.logger.with_rescue_and_duration_worker(correlation_id, worker.class.name, cloned_args[0], worker.jid) do
-        worker.perform(*cloned_args)
-      end
+Sidekiq::Processor.class_eval do
+  # alias_method :original_execute_job, :execute_job
+  def execute_job(worker, cloned_args)
+    correlation_id = cloned_args[0]["correlation_id"]
+    Application.logger.with_rescue_and_duration_worker(correlation_id, worker.class.name, cloned_args[0], worker.jid) do
+      worker.perform(*cloned_args)
     end
   end
 end
