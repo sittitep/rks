0dbaaac4714294091d1a7528f56f03158ee29249
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2018-11-21 00:54:24 +0700

    Remove unused code

--- lib/rks/worker.rb
+++ lib/rks/worker.rb
@@ -2,9 +2,5 @@ module RKS
   class Worker
     include Sidekiq::Worker
     sidekiq_options retry: 3, queue: :normal
-
-    def self.children
-      ObjectSpace.each_object(Class).select { |klass| klass < self }
-    end
   end
 end
